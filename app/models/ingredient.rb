153c1a19a49483bcb51e8a887bb4f879b1b773be
Agathe Badia <ab@honeypot.io>
2019-11-14 12:42:00 +0100

    Models ready

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,5 @@
 class Ingredient < ApplicationRecord
-  has_many :doses, dependent: :destroy
-  has_many :cocktails, through: :doses
+  has_many :doses
 
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, uniqueness: true, presence: true
 end
